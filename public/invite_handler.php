d0f7b172ad374173ca2b3fb4aa3c9da5cef6267f
Dylan Murphey <dylanpmurphey@gmail.com>
2024-03-28 16:52:05 -0600

    no duplicate adds :)

--- public/invite_handler.php
+++ public/invite_handler.php
@@ -19,8 +19,8 @@
         $inv = $dao->getInvite($invite_id);
 
         if ($_SESSION['user_id'] == $inv['recipient_id']) {
-            if ($accept) {
-                echo $dao->addUserToLadder($inv['recipient_id'],$inv['ladder_id']);
+            if ($accept && $dao->checkUserInTable($_SESSION['user_id'],$inv['ladder_id'])) {
+                $dao->addUserToLadder($inv['recipient_id'],$inv['ladder_id']);
             }
             $dao->deleteInvite($invite_id);
             leave("ladders.php?view_ladder={$inv['ladder_id']}");
