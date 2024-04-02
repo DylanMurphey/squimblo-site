dc8b3b6d6cc15fa3d1af7a451e755598f99cad57
Dylan Murphey <dylanpmurphey@gmail.com>
2024-04-02 00:40:43 -0600

    rearrange login so password managers work better

--- public/login.php
+++ public/login.php
@@ -12,22 +12,6 @@
 ?>
 
 <div class="login-wrapper">
-    <div class="login-container" id="login-field">
-        <form method="post" action="login_handler.php">
-            <label for="username"><b>Existing user</b><br /></label>
-            <?php
-                if(isset($_SESSION['warning']) && isset($_SESSION['warning']['login'])){
-                    echo "<p class='warning'>{$_SESSION['warning']['login']}</p>";
-                }
-            ?>
-            <input type="text" placeholder="Username" name="username"
-                <?php if(isset($_SESSION['prefill']) && isset($_SESSION['prefill']['login_username'])){ echo "value='{$_SESSION['prefill']['login_username']}'"; } ?>
-                required><br />
-            <input type="password" placeholder="Password" name="password" required><br />
-            <button type="submit" value="Login">Login</button>
-        </form>
-    </div>
-
     <div class="login-container" id="signup-field">
         <form method="post" action="registration_handler.php">
             <label for="username"><b>New user</b><br /></label>
@@ -45,6 +29,22 @@
             <input type="password" placeholder="Password" name="password" required><br />
             <input type="password" placeholder="Confirm Password" name="password_confirm" required><br />
             <button type="submit">Sign up</button>
+        </form>
+    </div>
+    
+    <div class="login-container" id="login-field">
+        <form method="post" action="login_handler.php">
+            <label for="username"><b>Existing user</b><br /></label>
+            <?php
+                if(isset($_SESSION['warning']) && isset($_SESSION['warning']['login'])){
+                    echo "<p class='warning'>{$_SESSION['warning']['login']}</p>";
+                }
+            ?>
+            <input type="text" placeholder="Username" name="username"
+                <?php if(isset($_SESSION['prefill']) && isset($_SESSION['prefill']['login_username'])){ echo "value='{$_SESSION['prefill']['login_username']}'"; } ?>
+                required><br />
+            <input type="password" placeholder="Password" name="password" required><br />
+            <button type="submit" value="Login">Login</button>
         </form>
     </div>
 </div>
